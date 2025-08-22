f5a3f1c0ed8e68d92f160e50e07c1733b88ccc71
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-22 09:47:44 -0600

    Jupyter act

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/davo/Programacion/python/venv
+set -gx VIRTUAL_ENV /home/davo/Programacion/redes-neuronales/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
